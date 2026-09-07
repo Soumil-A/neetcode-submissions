66d8c606906a4fb83fb62a6f942e8f90f0d7cf42
Soumil-A <soumil.asanare@utexas.edu>
2026-09-07 19:06:11 -0400

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        filtered = []
+        for char in s:
+            if char.isalnum():
+                filtered.append(char.lower()) 
+
+        return filtered == filtered[::-1]
+
+
+        
